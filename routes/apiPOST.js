6e9275429742ad6b391cb15d30bac900d86c3308
Ari Gibson <arimgibson@gmail.com>
2020-07-17 05:29:02 -0600

    Added administrator POST

--- routes/apiPOST.js
+++ routes/apiPOST.js
@@ -37,7 +37,7 @@ api.post("/api/business/id/:id", function (req, res) {
     )
     .then((place) => {
       review
-        .get("food")
+        .get("businesses")
         .push({
           place_id: req.params.id,
           name: place.data.result.name,
@@ -59,6 +59,43 @@ api.post("/api/business/id/:id", function (req, res) {
       });
     })
     .catch((err) => console.log(err));
+});
+
+api.post("/api/business/id-pass/:pass/:id", function (req, res) {
+  if (req.params.pass == process.env.PASS) {
+    axios
+      .get(
+        `https://maps.googleapis.com/maps/api/place/details/json?place_id=${req.params.id}&fields=name,formatted_address,formatted_phone_number,types,website,url&key=AIzaSyDRjM-tDxDM3Ji0YUm-b-KUU_aPo4nmweM`,
+        {
+          method: "GET",
+          headers: { "Content-Type": "application/json" },
+        }
+      )
+      .then((place) => {
+        accepted
+          .get("businesses")
+          .push({
+            place_id: req.params.id,
+            name: place.data.result.name,
+            address: place.data.result.address,
+            phone: place.data.result.formatted_phone_number,
+            type: place.data.result.types,
+            website: place.data.result.website,
+            google_url: place.data.result.url,
+          })
+          .write();
+        res.status(200).json({
+          place_id: req.params.id,
+          name: place.data.result.name,
+          address: place.data.result.address,
+          phone: place.data.result.formatted_phone_number,
+          type: place.data.result.types,
+          website: place.data.result.website,
+          google_url: place.data.result.url,
+        });
+      })
+      .catch((err) => console.log(err));
+  } else {res.status(401).send("Invalid administrator password.")}
 });
 
 module.exports = api;
