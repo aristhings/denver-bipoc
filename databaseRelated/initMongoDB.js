3bf8e07f5125fdb8feb47de340bd15f21417b944
Ari Gibson <arimgibson@gmail.com>
2020-07-24 18:52:56 -0600

    Removed bad things

--- databaseRelated/initMongoDB.js
+++ databaseRelated/initMongoDB.js
@@ -1,13 +1,11 @@
 const MongoClient = require("mongodb").MongoClient;
-const url =
-  "mongodb+srv://ari:fz8aiTY%2454016mc2b%24*%26@businesses.ophxv.gcp.mongodb.net/business?retryWrites=true&w=majority";
 
 var _db;
 
 module.exports = {
   connectToServer: function (callback) {
     MongoClient.connect(
-      url,
+      process.env.mongoDB,
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
